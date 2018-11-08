eb7a10d01838f0dcb76b7f2eac7f6d1dc3492032
Bowei Liu <liubowei@gmail.com>
2018-11-08 07:33:58 +0000

    wayyy beter mobile

--- index.js
+++ index.js
@@ -16,21 +16,48 @@ app.use(cookieParser())
 const INDEX_STRING = 
 `
 <style>
-table, th, td {
+.navbar {
+  position: fixed;
+  background-color: #FFFFFF;
+  top: 0;
+  width: 100%;
+}
+.main {
+  margin-top: 160px;
+}
+th, td {
     border: 1px solid black;
     border-collapse: collapse;
+__MOBILE__:    font-size: 24px;
+}
+table {
+__MOBILE__:    table-layout: fixed;
+__MOBILE__:    width: 100%;
+    border: 1px solid black;
+    border-collapse: collapse;
+__MOBILE__:    font-size: 24px;
 }
 th, td {
     padding: 5px;
 }
 </style>
 
-<form action="/__GAME_ID__" method="post">
+<div class="navbar">
+__MOBILE__: Enter command below: <br>
+__MOBILE__:<textarea rows="5" cols="30" name="command" form="myform" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">
+__MOBILE__:</textarea> <br>
+<form action="/__GAME_ID__" method="post" id="myform">
   <input type="submit" value="Play">
-  <input type="text" name="command" value="__COMMAND__"><br>
-</form> 
+__DESKTOP__:<input type="text" name="command" value="__COMMAND__"><br>
+</form>
+</div>
 
+__MOBILE__:<body style="font-size:16px">
+__DESKTOP__:<body>
+<div class="main">
 __BODY__
+</div>
+</body>
 `
 
 const HELP_TABLE = (() => {
@@ -58,7 +85,8 @@ const HELP_TABLE = (() => {
 })()
 const SHOW_COMMANDS_REGEX = /^(HELP|[HIJKLMOPSV]*)$/
 //HIJLMOPSV
-const JEWELS_ROW = ['[Q]uartz', '[W]olframite', '[E]merald', '[R]uby', '[T]urquoise', '[G]old' ]
+const JEWELS_ROW_FULL = ['[Q]uartz', '[W]olframite', '[E]merald', '[R]uby', '[T]urquoise', '[G]old' ]
+const JEWELS_ROW_ABBR = ['Q', 'W', 'E', 'R', 'T', 'G'].map(c => '   ' + c + '   ')
 const jewel_to_idx = (ch) => {
     if (ch === 'Q') { return 0 }
     if (ch === 'W') { return 1 }
@@ -76,7 +104,7 @@ const to_html_table = (rows) => {
 }
 
 // TODO(bowei): permute the card list to be canonical. Currently the costs are in the order 01234-BWRGB and the resources by QWERT=black blue green red white (that's what the card list pdf gives).
-const CARD_LIST = require('./card_table.json')
+let CARD_LIST = require('./card_table.json')
 // TODO(bowei): permute the nobles to be canonical. Again costs are given in the order 01234-BWRGB
 const NOBLE_LIST = require('./nobles.json')
 
@@ -85,7 +113,10 @@ const all_game_states = {}
 const main = (req, res) => {
     let game_id = req.params.game_id; // how we keep track of game state
     let body = '' // what to return
-    console.log('user agent', req.get('User-Agent')) // used to detect curl
+    let user_agent = req.get('User-Agent')
+    const is_mobile = true || (function(a){ return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)) })(user_agent)
+    console.log('\nuser agent', user_agent, {is_mobile} ) // can also be used to detect curl
+    const JEWELS_ROW = is_mobile ? JEWELS_ROW_ABBR : JEWELS_ROW_FULL
     console.log('remote ip', req.headers['x-forwarded-for'] || req.connection.remoteAddress) // not sure why we would need this
     //console.log('cookies', req.cookies)
     let user_id
@@ -100,6 +131,7 @@ const main = (req, res) => {
 
     let command = (req.body && req.body.command) || ''
     console.log({ game_id, command })
+    command = command.trim().replace(/\r/g,'').replace(/\n/g,'').replace(/;{2,}/g,';')
     // Default entry screen, if no command was given or if game is not started yet
     if (!command || !all_game_states[game_id]) {
         if (!all_game_states[game_id]) {
@@ -316,11 +348,11 @@ const main = (req, res) => {
                 body += `<br>You have a total of ${_.sum(game_state.inventory[current_player])} jewels, ${game_state.purchased[current_player].length} buildings, and ${game_state.reserves[current_player].length} reserved cards.`
             } else if (chr === 'J') {
                 body += 'Sorting market by jewels: <br>'
-                rows.push((['Card ID']).concat(JEWELS_ROW.slice(0,5)).concat(['Provides'], ['VP']))
                 rows = rows.concat(CARD_KEYS.map(k => [k].concat(game_state.market[k])))
                 rows = rows.concat(game_state.reserves[current_player].map((r, i) => ['X' + (i+1).toString()].concat(r)))
                 rows = rows.concat(game_state.reserves[other_player].map((r, i) => ['O' + (i+1).toString()].concat(r)))
                 rows = _.sortBy(rows, r => jewel_to_idx(r[6]) * 10 + r[0])
+                rows.unshift((['Card ID']).concat(JEWELS_ROW.slice(0,5)).concat(['Gem'], ['VP']))
                 body += to_html_table(rows)
             } else if (chr === 'K') {
                 body += 'Chat: <br>'
@@ -348,11 +380,11 @@ const main = (req, res) => {
                     return r.concat([_.sum(out_of_pocket_cost), Math.max(0, _.sum(how_many_gems_short) - game_state.inventory[other_player][5] ) ])
                 })
                 rows = _.sortBy(rows, ['8', '9']) // oop cost, then current cost
-                rows.unshift((['Card ID']).concat(JEWELS_ROW.slice(0,5)).concat(['Provides', 'VP', 'Out of pocket cost', 'How many gems short', 'Enemy\'s OOPC', 'Enemy\'s HMGS']))
+                rows.unshift((['Card ID']).concat(JEWELS_ROW.slice(0,5)).concat(['Gem', 'VP', is_mobile ? 'Cost' : 'Out of pocket cost', is_mobile ? 'Short?' : 'How many gems short', is_mobile ? 'Opp. cost' : 'Enemy\'s OOPC', is_mobile ? 'Opp. short' : 'Enemy\'s HMGS']))
                 body += to_html_table(rows)
             } else if (chr === 'M') {
                 body += 'Cards available on the market: <br>'
-                rows.push((['Card ID']).concat(JEWELS_ROW.slice(0,5)).concat(['Provides'], ['VP']))
+                rows.push((['Card ID']).concat(JEWELS_ROW.slice(0,5)).concat(['Gem'], ['VP']))
                 rows = rows.concat(CARD_KEYS.map(k => [k].concat(game_state.market[k])))
                 rows = rows.concat(game_state.reserves[current_player].map((r, i) => ['X' + (i+1).toString()].concat(r)))
                 rows = rows.concat(game_state.reserves[other_player].map((r, i) => ['O' + (i+1).toString()].concat(r)))
@@ -369,9 +401,10 @@ const main = (req, res) => {
             } else if (chr === 'P') {
                 body += 'Players: <br>'
                 if (!game_state.players.p2) {
-                    body += 'Waiting for a second player... <br>'
+                    body += 'Waiting for a second player...'
                 } else {
-                    body += `You have ${game_state.points[current_player]} points, your opponent has ${game_state.points[other_player]}. <br>`
+                    body += `<br> You have ${game_state.points[current_player]} points, your opponent has ${game_state.points[other_player]}.`
+                    // TODO(bowei): make player 2 have an extra turn here if player 1 just "won"!
                     if (game_state.whose_turn === 'gg') {
                         body += 'The game is over!'
                     } else if (game_state.whose_turn === current_player) {
@@ -379,7 +412,7 @@ const main = (req, res) => {
                     } else {
                         body += 'It is not your turn. '
                     }
-                    body += `You are player ${current_player === 'p1' ? '1' : '2'}.`
+                    body += `You are player ${current_player === 'p1' ? '1' : (current_player === 'p2' ? '2' : 'spectator')}.`
                 }
             } else if (chr === 'S') {
                 body += 'Stocks: <br>'
@@ -390,7 +423,7 @@ const main = (req, res) => {
                 body += 'Available and acquired victory points: <br>'
                 rows.push((['']).concat(JEWELS_ROW.slice(0,5)).concat(['Owned by', 'VP']))
                 // TODO(bowei): filter owned nobles to the bottom
-                rows = rows.concat(game_state.nobles.map((r,i) => ['Noble ' + (i+1).toString()].concat(r).concat([3])))
+                rows = rows.concat(game_state.nobles.map((r,i) => [(is_mobile ? 'N' : 'Noble ') + (i+1).toString()].concat(r).concat([3])))
                 let to_concat = CARD_KEYS.map(k => [k].concat(game_state.market[k]))
                 to_concat = to_concat.concat(game_state.reserves[current_player].map((r, i) => ['X' + (i+1).toString()].concat(r)))
                 to_concat = _(to_concat).filter(r => r[7] > 0).map(r => {
@@ -417,7 +450,13 @@ const main = (req, res) => {
     let to_ret = INDEX_STRING
     to_ret = to_ret.replace('__GAME_ID__', game_id)
     to_ret = to_ret.replace('__BODY__', body)
-    to_ret = to_ret.replace('__COMMAND__', command)
+    to_ret = to_ret.replace(/__COMMAND__/g, command)
+    to_ret = to_ret.replace('__FONT_SIZE__', is_mobile ? 32 : 16 )
+    if (is_mobile) {
+        to_ret = to_ret.split('\n').filter(l => !/^__DESKTOP__:/.test(l)).map(l => l.replace('__MOBILE__:','')).join('\n')
+    } else {
+        to_ret = to_ret.split('\n').filter(l => !/^__MOBILE__:/.test(l)).map(l => l.replace('__DESKTOP__:','')).join('\n')
+    }
     return res.send(to_ret)
 }
 
